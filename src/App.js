bfca0f922b1fb5d30056a0cc36bb0d55c1ccea71
Daniel Palmer <daniel.palmer@shaw.ca>
2022-11-07 22:34:09 -0700

    reset test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import About from './pages/About';
 import Invite from './pages/Invite';
 import Commands from './pages/Commands';
 import Contact from './pages/Contact';
-import World from './pages/World'
 import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom'
 
 function App() {
@@ -20,14 +19,12 @@ function App() {
                     <NavLink to="/invite" className="navbarContent" activeClassName="navbarActive">Invite</NavLink>
                     <NavLink to="/commands" className="navbarContent" activeClassName="navbarActive">Commands</NavLink>
                     <NavLink to="/contact" className="navbarContent" activeClassName="navbarActive">Contact</NavLink>
-                    <NavLink to="/world" className="navbarContent" activeClassName="navbarActive">World</NavLink>
                 </div>
                 <Switch>
                     <Route path="/" exact={true} component={About}/>
                     <Route path="/invite" exact={true} component={Invite}/>
                     <Route path="/commands" exact={true} component={Commands}/>
                     <Route path="/contact" exact={true} component={Contact}/>
-                    <Route path="/world" exact={true} component={World} />
                 </Switch>
             </div>
         </Router>
